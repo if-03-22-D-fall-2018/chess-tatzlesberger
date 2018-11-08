ec33b1e959267271fffbc8da4f81525904612b74
tatzlesberger <43432712+tatzlesberger@users.noreply.github.com>
2018-11-08 08:44:08 +0100

    Update chess.cpp

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -10,3 +10,18 @@
  * Implementation of basic chess functions.
  * ----------------------------------------------------------
  */
+#include "chess.h"
+#include <stdio.h>
+
+void initaialize(struct ChessSquare board[8][8])
+{
+    board [0] [0].is_set = true;
+    board [0] [0].piece.color = BLACK;
+    board [0] [0].piece.type = ROOK;
+    printf("Initaialize\n");
+}
+
+void print_board(struct ChessSquare board[8][8])
+{
+    printf("Printed\n");
+}
