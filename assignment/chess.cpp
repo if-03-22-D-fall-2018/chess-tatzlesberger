e3426a31a5bff273c71b5ca9c37d0ec9a6bc2874
tatzlesberger <t.atzlesberger@gmx.at>
2018-11-13 13:12:42 +0100

    bepis

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -25,3 +25,82 @@ void print_board(struct ChessSquare board[8][8])
 {
     printf("Printed\n");
 }
+
+void init_chess_board(ChessBoard chess_board)
+{
+
+}
+void print_board(struct ChessSquare board[8][8])
+{
+
+}
+
+bool is_piece(struct ChessPiece chess_piece, enum Color color, enum PieceType piece_type)
+{
+
+}
+
+struct ChessSquare* get_square(ChessBoard chess_board, File file, Rank rank)
+{
+
+}
+
+bool is_square_occupied(ChessBoard chess_board, File file, Rank rank)
+{
+
+}
+
+bool add_piece(ChessBoard chess_board, File file, Rank rank, struct ChessPiece chess_piece)
+{
+
+}
+
+struct ChessPiece get_piece(ChessBoard chess_board, File file, Rank rank)
+{
+
+}
+
+void setup_chess_board(ChessBoard chess_board)
+{
+
+}
+
+bool remove_piece(ChessBoard chess_board, File file, Rank rank)
+{
+
+}
+
+bool squares_share_file(File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+
+}
+
+bool squares_share_rank(File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+
+}
+
+bool squares_share_diagonal(File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+
+}
+
+bool squares_share_knights_move(File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+
+}
+
+bool squares_share_pawns_move(enum Color color, enum MoveType move_type, File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+
+}
+
+bool squares_share_queens_move(File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+
+}
+
+bool squares_share_kings_move(File fileOne, Rank rankOne, File fileTwo, Rank rankTwo)
+{
+  
+}
